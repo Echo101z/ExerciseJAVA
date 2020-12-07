cef5bc3cc9bef92cdaf7a0b2190634408e99f652
Kshitij Porwal <kshitijporwal88@gmail.com>
2020-12-07 13:00:44 +0530

    added getter and setter

--- Mobile/src/Mobile.java
+++ Mobile/src/Mobile.java
@@ -3,6 +3,46 @@ class Mobile{
 	static String company="XYZ";
 	String name;
 	String plan;
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getPlan() {
+		return plan;
+	}
+
+
+	public void setPlan(String plan) {
+		this.plan = plan;
+	}
+
+
+	public long getNumber() {
+		return number;
+	}
+
+
+	public void setNumber(long number) {
+		this.number = number;
+	}
+
+
+	public int getAmount() {
+		return amount;
+	}
+
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+
 	long number;
 	int amount;
 	
